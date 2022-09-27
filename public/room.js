addcdbc15597df94e99b7e12afb8236580180bb4
Nafaarts <71387948+nafaarts@users.noreply.github.com>
2022-09-28 03:55:54 +0700

    fix redirect #6

--- public/room.js
+++ public/room.js
@@ -39,24 +39,24 @@ function createPeer(userIdToCall, i) {
         username: "YzYNCouZM1mhqhmseWk6",
         credential: "YzYNCouZM1mhqhmseWk6",
       },
-      {
-        urls: "stun:openrelay.metered.ca:80",
-      },
-      {
-        urls: "turn:openrelay.metered.ca:80",
-        username: "openrelayproject",
-        credential: "openrelayproject",
-      },
-      {
-        urls: "turn:openrelay.metered.ca:443",
-        username: "openrelayproject",
-        credential: "openrelayproject",
-      },
-      {
-        urls: "turn:openrelay.metered.ca:443?transport=tcp",
-        username: "openrelayproject",
-        credential: "openrelayproject",
-      },
+      // {
+      //   urls: "stun:openrelay.metered.ca:80",
+      // },
+      // {
+      //   urls: "turn:openrelay.metered.ca:80",
+      //   username: "openrelayproject",
+      //   credential: "openrelayproject",
+      // },
+      // {
+      //   urls: "turn:openrelay.metered.ca:443",
+      //   username: "openrelayproject",
+      //   credential: "openrelayproject",
+      // },
+      // {
+      //   urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      //   username: "openrelayproject",
+      //   credential: "openrelayproject",
+      // },
     ],
   })
 
